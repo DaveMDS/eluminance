c6f9333b11d62921f567a1ec06c95fffa476f501
Dave Andreoli <dave@gurumeditation.it>
2015-05-08 20:21:38 +0200

    Check EFL version in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.version import StrictVersion
 from efl.utils.setup import build_extra, build_edc, build_fdo, uninstall
+from efl import __version__ as efl_version
 
+MIN_EFL = '1.14'
+if StrictVersion(efl_version) < MIN_EFL:
+    print('Your python-efl version is too old! Found: ' + efl_version)
+    print('You need at least version ' + MIN_EFL)
+    exit(1)
 
 setup(
     name = 'eluminance',
-    version = '0.8',
+    version = '0.9',
     description = 'Photo browser',
     long_description = 'A photo browser written in python-efl',
     license = "GNU GPL",
